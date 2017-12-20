675f24343d3c8d9a96e1bed7c48592a22ef0c1a5
FragJage <fitrec.jage@free.fr>
2017-12-20 22:56:16 +0100

    Example 2

--- examples/example2.cpp
+++ examples/example2.cpp
@@ -7,6 +7,7 @@
 /***********************************************************************************************/
 
 #include <iostream>
+#include <iomanip>
 #include "LightTSDB.h"
 
 using namespace std;
@@ -21,6 +22,14 @@ int main()
 
 
     myTSDB.SetFolder("test/data");
+
+    localtime_r(&start, &tmtime);
+    cout << setfill('0');
+    cout << "Read data for the day " << 1900+tmtime.tm_year << "/" << setw(2) << tmtime.tm_mon << "/" << setw(2) << tmtime.tm_mday;
+    cout << " between " <<  setw(2) << tmtime.tm_hour << ":00 and " <<  setw(2) << tmtime.tm_hour << ":59" << endl;
+
+    cout << endl;
+    cout << "### All data ############################################" << endl;
     if(!myTSDB.ReadValues("LucileBedRoomTemperature", start, myValues))
     {
         myError = myTSDB.GetLastError("LucileBedRoomTemperature");
@@ -28,12 +37,27 @@ int main()
         return -1;
     }
 
-    localtime_r(&start, &tmtime);
-    cout << "All data for the day " << 1900+tmtime.tm_year << "/" << tmtime.tm_mon << "/" << tmtime.tm_mday << endl;
     for(list<LightTSDB::DataValue>::const_iterator it=myValues.begin(); it!=myValues.end(); ++it)
     {
         localtime_r(&(it->time), &tmtime);
-        cout << tmtime.tm_hour << ":" << tmtime.tm_min << ":" << tmtime.tm_sec << " => " << it->value.Float << endl;
+        cout << setw(2) << tmtime.tm_hour << ":" << setw(2) << tmtime.tm_min << ":" << setw(2) << tmtime.tm_sec;
+        cout << " => " << it->value.Float << endl;
+    }
+
+    cout << endl;
+    cout << "### Resample data to have a value for all 5 min #########" << endl;
+    if(!myTSDB.ResampleValues("LucileBedRoomTemperature", start, start+3599, myValues, 300))
+    {
+        myError = myTSDB.GetLastError("LucileBedRoomTemperature");
+        cout << "ERROR" << endl << myError.ErrMessage << endl << myError.SysMessage << endl;
+        return -1;
+    }
+
+    for(list<LightTSDB::DataValue>::const_iterator it=myValues.begin(); it!=myValues.end(); ++it)
+    {
+        localtime_r(&(it->time), &tmtime);
+        cout << setw(2) << tmtime.tm_hour << ":" << setw(2) << tmtime.tm_min << ":" << setw(2) << tmtime.tm_sec;
+        cout << " => " << it->value.Float << endl;
     }
 
     return 0;
