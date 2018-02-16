703fcb0d35f76675e328953f88bc362ee8beecaf
GELLEZ <fgellez@cegid.fr>
2018-02-16 13:35:31 +0100

    Add licence

--- server/WelcomeController.cpp
+++ server/WelcomeController.cpp
@@ -1,3 +1,23 @@
+/*** LICENCE ***************************************************************************************/
+/*
+  LightTSDB - Light time series database
+  
+  This file is part of LightTSDB server.
+  
+    LightTSDB is free software : you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+	
+    LightTSDB is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+	
+    You should have received a copy of the GNU General Public License
+    along with LightTSDB.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/***************************************************************************************************/
 #include "WelcomeController.h"
 
 using namespace std;
